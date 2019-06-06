c1b7d3f5434ef3dc2b2c8dfd612c3c6d669b2cf6
Vitor <v.lopes@loans2go.co.uk>
2019-06-06 09:46:49 +0100

    Initial Commit

---
+++ ConsoleApp1/HelloService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class HelloService : IHelloService
+    {
+        public void SayHello()
+        {
+            Console.Write("Hello !");
+            Console.Read();
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var container = new UnityContainer();
+            container.RegisterType<IHelloService, HelloService>();
+
+            var serviceInstance = container.Resolve<IHelloService>();
+
+            serviceInstance.SayHello();
+        }
+    }
+}
